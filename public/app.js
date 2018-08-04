81a74ad0a12e2f4199f2fe806129aa7ecd1e83c4
Rahul Kumar <gurusfrontend@gmail.com>
2018-08-05 01:36:38 +0530

    task tracker app

--- public/app.js
+++ public/app.js
@@ -2,78 +2,111 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+// parent component:- TaskList
 var TaskList = function (_React$Component) {
     _inherits(TaskList, _React$Component);
 
-    function TaskList() {
+    function TaskList(props) {
         _classCallCheck(this, TaskList);
 
-        return _possibleConstructorReturn(this, (TaskList.__proto__ || Object.getPrototypeOf(TaskList)).apply(this, arguments));
+        var _this = _possibleConstructorReturn(this, (TaskList.__proto__ || Object.getPrototypeOf(TaskList)).call(this, props));
+
+        _this.state = {
+            taskListData: [],
+            showFormError: false
+        };
+
+        _this.handleAddTask = _this.handleAddTask.bind(_this);
+        _this.handleRemoveTask = _this.handleRemoveTask.bind(_this);
+
+        return _this;
     }
 
     _createClass(TaskList, [{
+        key: 'handleRemoveTask',
+        value: function handleRemoveTask(option) {
+            // this.setState((prevstate)=> ({
+            //     taskListData: prevstate.taskListData.filter((el)=>{
+            //         console.log(el !== option)
+            //         return el !== option
+            //     })
+            // }))
+
+            var array = [].concat(_toConsumableArray(this.state.taskListData));
+            var index = array.indexOf(option.target.value);
+            array.splice(index, 1);
+            this.setState({ taskListData: array });
+        }
+    }, {
+        key: 'handleAddTask',
+        value: function handleAddTask(e) {
+            e.preventDefault();
+            var taskname = e.target.elements.taskName.value.trim();
+            var taskdate = e.target.elements.taskDate.value.trim();
+            var assignedname = e.target.elements.assignedName.value.trim();
+            var formvalues = {};
+            formvalues.taskName = taskname;
+            formvalues.date = taskdate;
+            formvalues.assignedTo = assignedname;
+
+            if (!taskname == '' && !taskdate == '' && !assignedname == '') {
+                this.setState(function (prevstate) {
+                    return {
+                        taskListData: prevstate.taskListData.concat(formvalues),
+                        showFormError: false
+                    };
+                });
+
+                e.target.elements.taskName.value = '';
+                e.target.elements.taskDate.value = '';
+                e.target.elements.assignedName.value = '';
+            } else {
+                this.setState(function () {
+                    return {
+                        showFormError: true
+                    };
+                });
+            }
+        }
+    }, {
         key: 'render',
         value: function render() {
+            var formError = this.state.showFormError;
+            var showAddTaskMsg = this.state.taskListData;
+            var checkTaskLength = showAddTaskMsg.length;
+
             return React.createElement(
                 'div',
                 { className: 'tasklist-container' },
-                React.createElement(
-                    'div',
-                    { className: 'main-head' },
-                    'Task Tracker'
-                ),
+                React.createElement(Head, { title: 'Task Tracker', headClass: 'main-head' }),
                 React.createElement(
                     'div',
                     { className: 'display-flex' },
                     React.createElement(
                         'div',
                         { className: 'create-task' },
-                        React.createElement(
-                            'div',
-                            { className: 'section-head' },
-                            'Create a Task'
-                        ),
+                        React.createElement(Head, { title: 'Create a Task', headClass: 'section-head' }),
                         React.createElement(
                             'div',
                             { className: 'form-container' },
                             React.createElement(
                                 'form',
-                                null,
-                                React.createElement(
+                                { onSubmit: this.handleAddTask },
+                                React.createElement(Formfield, { label: 'Task Name', fieldtype: 'text', placeholder: 'Please enter task name', name: 'taskName' }),
+                                React.createElement(Formfield, { label: 'Date', fieldtype: 'date', placeholder: 'Please select date', name: 'taskDate' }),
+                                React.createElement(Formfield, { label: 'Assigned to', fieldtype: 'text', placeholder: 'Please enter name', name: 'assignedName' }),
+                                formError && React.createElement(
                                     'div',
-                                    { className: 'field-row' },
-                                    React.createElement(
-                                        'label',
-                                        null,
-                                        'Task Name'
-                                    ),
-                                    React.createElement('input', { type: 'text', className: 'form-control', placeholder: 'Today Task' })
-                                ),
-                                React.createElement(
-                                    'div',
-                                    { className: 'field-row' },
-                                    React.createElement(
-                                        'label',
-                                        null,
-                                        'Date'
-                                    ),
-                                    React.createElement('input', { type: 'date', placeholder: 'Select Date', className: 'form-control' })
-                                ),
-                                React.createElement(
-                                    'div',
-                                    { className: 'field-row' },
-                                    React.createElement(
-                                        'label',
-                                        null,
-                                        'Assigned to'
-                                    ),
-                                    React.createElement('input', { type: 'text', placeholder: 'Please enter name', className: 'form-control' })
+                                    { className: 'form-error' },
+                                    'Please fill all the form fields'
                                 ),
                                 React.createElement(
                                     'button',
@@ -86,54 +119,13 @@ var TaskList = function (_React$Component) {
                     React.createElement(
                         'div',
                         { className: 'task-list' },
-                        React.createElement(
+                        React.createElement(Head, { title: 'Existing Tasks', headClass: 'section-head' }),
+                        checkTaskLength == 0 && React.createElement(
                             'div',
-                            { className: 'section-head' },
-                            'Existing Tasks'
+                            { className: 'no-task' },
+                            'There is no existing task'
                         ),
-                        React.createElement(
-                            'div',
-                            { className: 'tasklist-indent' },
-                            React.createElement(
-                                'div',
-                                { className: 'tasklist-row' },
-                                React.createElement(
-                                    'div',
-                                    { classname: 'col' },
-                                    React.createElement(
-                                        'span',
-                                        null,
-                                        'Task Name'
-                                    ),
-                                    'Test Task'
-                                ),
-                                React.createElement(
-                                    'div',
-                                    { className: 'col' },
-                                    React.createElement(
-                                        'span',
-                                        null,
-                                        'Assigned Date'
-                                    ),
-                                    '20/05/2018'
-                                ),
-                                React.createElement(
-                                    'div',
-                                    { className: 'col' },
-                                    React.createElement(
-                                        'span',
-                                        null,
-                                        'Assigned to'
-                                    ),
-                                    'Michael Abitbol'
-                                ),
-                                React.createElement(
-                                    'div',
-                                    { className: 'remove' },
-                                    'x'
-                                )
-                            )
-                        )
+                        checkTaskLength > 0 && React.createElement(TaskListContainer, { taskListContent: this.state.taskListData, handleRemoveTask: this.handleRemoveTask })
                     )
                 )
             );
@@ -143,5 +135,163 @@ var TaskList = function (_React$Component) {
     return TaskList;
 }(React.Component);
 
-var app = document.getElementById('app');
+// Head component
+
+
+var Head = function (_React$Component2) {
+    _inherits(Head, _React$Component2);
+
+    function Head() {
+        _classCallCheck(this, Head);
+
+        return _possibleConstructorReturn(this, (Head.__proto__ || Object.getPrototypeOf(Head)).apply(this, arguments));
+    }
+
+    _createClass(Head, [{
+        key: 'render',
+        value: function render() {
+            var headClass = this.props.headClass;
+            return React.createElement(
+                'div',
+                { className: headClass },
+                this.props.title
+            );
+        }
+    }]);
+
+    return Head;
+}(React.Component);
+
+//FormField Component
+
+
+var Formfield = function (_React$Component3) {
+    _inherits(Formfield, _React$Component3);
+
+    function Formfield() {
+        _classCallCheck(this, Formfield);
+
+        return _possibleConstructorReturn(this, (Formfield.__proto__ || Object.getPrototypeOf(Formfield)).apply(this, arguments));
+    }
+
+    _createClass(Formfield, [{
+        key: 'render',
+        value: function render() {
+
+            return React.createElement(
+                'div',
+                { className: 'field-row' },
+                React.createElement(
+                    'label',
+                    null,
+                    this.props.label
+                ),
+                React.createElement('input', { type: this.props.fieldtype, className: 'form-control', name: this.props.name, placeholder: this.props.placeholder })
+            );
+        }
+    }]);
+
+    return Formfield;
+}(React.Component);
+
+//TaskList Component
+
+
+var TaskListContainer = function (_React$Component4) {
+    _inherits(TaskListContainer, _React$Component4);
+
+    function TaskListContainer() {
+        _classCallCheck(this, TaskListContainer);
+
+        return _possibleConstructorReturn(this, (TaskListContainer.__proto__ || Object.getPrototypeOf(TaskListContainer)).apply(this, arguments));
+    }
+
+    _createClass(TaskListContainer, [{
+        key: 'render',
+        value: function render() {
+            var _this5 = this;
+
+            return React.createElement(
+                'div',
+                { className: 'tasklist-indent' },
+                this.props.taskListContent.map(function (data, index) {
+                    return React.createElement(TaskListRow, {
+                        key: index,
+                        taskName: data.taskName,
+                        assignedDate: data.date,
+                        assignedTo: data.assignedTo,
+                        handleRemoveTask: _this5.props.handleRemoveTask
+                    });
+                })
+            );
+        }
+    }]);
+
+    return TaskListContainer;
+}(React.Component);
+
+//TasListRow Component
+
+
+var TaskListRow = function (_React$Component5) {
+    _inherits(TaskListRow, _React$Component5);
+
+    function TaskListRow() {
+        _classCallCheck(this, TaskListRow);
+
+        return _possibleConstructorReturn(this, (TaskListRow.__proto__ || Object.getPrototypeOf(TaskListRow)).apply(this, arguments));
+    }
+
+    _createClass(TaskListRow, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { className: 'tasklist-row' },
+                React.createElement(
+                    'div',
+                    { className: 'col' },
+                    React.createElement(
+                        'div',
+                        null,
+                        'Task Name'
+                    ),
+                    this.props.taskName
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'col' },
+                    React.createElement(
+                        'div',
+                        null,
+                        'Assigned Date'
+                    ),
+                    this.props.assignedDate
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'col' },
+                    React.createElement(
+                        'div',
+                        null,
+                        'Assigned to'
+                    ),
+                    this.props.assignedTo
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'remove', onClick: this.props.handleRemoveTask },
+                    'Remove'
+                )
+            );
+        }
+    }]);
+
+    return TaskListRow;
+}(React.Component);
+
+//Init app
+
+
+var app = document.getElementById('taskTrackerApp');
 ReactDOM.render(React.createElement(TaskList, null), app);
--- src/app.js
+++ src/app.js
@@ -1,81 +1,173 @@
-class TaskList extends React.Component {
 
+// parent component:- TaskList
+class TaskList extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            taskListData: [],
+            showFormError: false
+        }
+
+        this.handleAddTask = this.handleAddTask.bind(this)
+        this.handleRemoveTask = this.handleRemoveTask.bind(this)
+
+    }
+
+    handleRemoveTask(option) {
+        // this.setState((prevstate)=> ({
+        //     taskListData: prevstate.taskListData.filter((el)=>{
+        //         console.log(el !== option)
+        //         return el !== option
+        //     })
+        // }))
+
+        var array = [...this.state.taskListData];
+        var index = array.indexOf(option.target.value)
+        array.splice(index, 1);
+        this.setState({taskListData: array});
+    }
+
+    handleAddTask(e) {
+        e.preventDefault();
+        const taskname = e.target.elements.taskName.value.trim()
+        const taskdate = e.target.elements.taskDate.value.trim()
+        const assignedname = e.target.elements.assignedName.value.trim()
+        const formvalues = {}
+        formvalues.taskName = taskname
+        formvalues.date = taskdate
+        formvalues.assignedTo = assignedname
+
+        if(!taskname == '' && !taskdate == '' && !assignedname == '') {
+            this.setState((prevstate)=>{
+                return {
+                    taskListData: prevstate.taskListData.concat(formvalues),
+                    showFormError: false
+                }
+            });
+
+            e.target.elements.taskName.value = ''
+            e.target.elements.taskDate.value = ''
+            e.target.elements.assignedName.value = ''
+
+        }else {
+            this.setState(()=>{
+                return {
+                    showFormError: true
+                }
+            });
+        }        
+    }
 
     render() {
+        const formError = this.state.showFormError
+        const showAddTaskMsg = this.state.taskListData
+        const checkTaskLength = showAddTaskMsg.length
+
         return (
             <div className={'tasklist-container'}>
-
-                <div className={'main-head'}>
-                    Task Tracker
-                </div>
-
+                <Head title={'Task Tracker'} headClass={'main-head'} />
                 <div className={'display-flex'}>
-                
                     <div className={'create-task'}>
-                        <div className={'section-head'}>Create a Task</div>
-
+                        <Head title={'Create a Task'} headClass={'section-head'} />
                         <div className={'form-container'}>
-                            <form>
-                                <div className={'field-row'}>
-                                    <label>Task Name</label>
-                                    <input type={'text'} className={'form-control'} placeholder={'Today Task'} />
-                                </div>
-
-                                <div className={'field-row'}>
-                                    <label>Date</label>
-                                    <input type={'date'} placeholder={'Select Date'} className={'form-control'} />
-                                </div>
-
-                                <div className={'field-row'}>
-                                    <label>Assigned to</label>
-                                    <input type={'text'} placeholder={'Please enter name'} className={'form-control'} />
-                                </div>
-
+                            <form onSubmit={this.handleAddTask}>
+                                <Formfield label={'Task Name'} fieldtype={'text'} placeholder={'Please enter task name'} name={'taskName'}  />
+                                <Formfield label={'Date'} fieldtype={'date'} placeholder={'Please select date'} name={'taskDate'}  />
+                                <Formfield label={'Assigned to'} fieldtype={'text'} placeholder={'Please enter name'} name={'assignedName'}  />
+                                {formError && <div className={'form-error'}>Please fill all the form fields</div>}
                                 <button type={'submit'}>Submit</button>
                             </form>
                         </div>
                     </div>
 
                     <div className={'task-list'}>
-                        <div className={'section-head'}>Existing Tasks</div>
-
-                        <div className={'tasklist-indent'}>
-                            <div className={'tasklist-row'}>
-                                <div classname={'col'}>
-                                    <span>Task Name</span>
-                                        Test Task
-                                </div>
-
-                                <div className={'col'}>
-                                    <span>Assigned Date</span>
-                                    20/05/2018
-                                </div>
-
-                                <div className={'col'}>
-                                    <span>Assigned to</span>
-                                    Michael Abitbol
-                                </div>
-
-                                <div className={'remove'}>
-                                    x
-                                </div>
-                            </div>
-                        </div>
-
+                        <Head title={'Existing Tasks'} headClass={'section-head'} />
+                         { checkTaskLength==0 && <div className={'no-task'}>There is no existing task</div>  }
+                         { checkTaskLength>0 && <TaskListContainer taskListContent={this.state.taskListData} handleRemoveTask={this.handleRemoveTask} /> }
                     </div>
                 </div>
-
-                
-
-
             </div>
         )
     }
-
-
 }
 
-const app = document.getElementById('app');
+// Head component
+class Head extends React.Component {
+    render() {
+        const headClass = this.props.headClass
+        return (
+            <div className={headClass}>
+                {this.props.title}
+            </div>
+        )
+    }
+}
+
+//FormField Component
+class Formfield extends React.Component {
+    render() {
+
+        return (
+            <div className={'field-row'}>
+                <label>{this.props.label}</label>
+                <input type={this.props.fieldtype} className={'form-control'} name={this.props.name} placeholder={this.props.placeholder} />
+            </div>
+        )
+    }
+}
+
+//TaskList Component
+class TaskListContainer extends React.Component {
+    render() {
+        return (
+            <div className={'tasklist-indent'}>
+                {
+                    this.props.taskListContent.map((data, index)=>{
+                        return <TaskListRow
+                            key={index}
+                            taskName={data.taskName}
+                            assignedDate={data.date}
+                            assignedTo={data.assignedTo}
+                            handleRemoveTask={this.props.handleRemoveTask}
+                        />
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+//TasListRow Component
+class TaskListRow extends React.Component {
+    render() {
+        return (
+            <div className={'tasklist-row'}>
+                <div className={'col'}>
+                    <div>Task Name</div>
+                        {this.props.taskName}
+                </div>
+
+                <div className={'col'}>
+                    <div>Assigned Date</div>
+                    {this.props.assignedDate}
+                </div>
+
+                <div className={'col'}>
+                    <div>Assigned to</div>
+                    {this.props.assignedTo}
+                </div>
+
+                <div className={'remove'} onClick={this.props.handleRemoveTask}>
+                    Remove
+                </div>
+            </div>
+        )
+    }
+}
+
+
+//Init app
+const app = document.getElementById('taskTrackerApp');
 ReactDOM.render(
     <TaskList />,
     app
